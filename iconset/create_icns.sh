30a510b294864bbdf2d324f48adc2bcb24fedc75
Dan Green <danngreen1@gmail.com>
2019-03-26 14:04:59 -0700

    Created iconset and ico file. Bash script to convert png to OSX iconset (requires OSX tool iconutil)

---
+++ iconset/create_icns.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+mkdir logo.iconset
+cd logo.iconset
+convert -resize 16x16 ../logo.png icon_16x16.png
+convert -resize 32x32 ../logo.png icon_16x16@2x.png
+convert -resize 32x32 ../logo.png icon_32x32.png
+convert -resize 64x64 ../logo.png icon_32x32@2x.png
+convert -resize 128x128 ../logo.png icon_128x128.png
+convert -resize 256x256 ../logo.png icon_128x128@2x.png
+convert -resize 256x256 ../logo.png icon_256x256.png
+convert -resize 512x512 ../logo.png icon_256x256@2x.png
+convert -resize 512x512 ../logo.png icon_512x512.png
+convert -resize 1024x1024 ../logo.png icon_512x512@2x.png
+cd ..
+iconutil -c icns logo.iconset
